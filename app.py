63c3f4329a2a274a78e7a283fe2de3bb321cc3df
KaramHassoun <mohamedkaram.hassoun@insat.ucar.tn>
2024-11-08 13:26:37 +0100

    Fix pytest

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def home():
-    return jsonify(message="Hello, World!")
+def hello_world():
+    return "Hello, World! This is a basic Flask app...."
 
 
 if __name__ == "__main__":
--- test_app.py
+++ test_app.py
@@ -1,8 +1,13 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from app import app
 
 
-def test_home():
+def test_hello_world():
     with app.test_client() as client:
         response = client.get("/")
         assert response.status_code == 200
-        assert response.get_json() == {"message": "Hello, World!"}
+        assert response.data == b"Hello, World! This is a basic Flask app...."
